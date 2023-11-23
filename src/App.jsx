27e626fd6aa93b8e304ed5900b079d9cffe1450b
Gloria Morales <gmoraleslondono@gmail.com>
2023-11-23 00:19:24 +0000

    Add React toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { Provider } from 'react-redux'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { tasks } from './redux/tasks'
+
+import { TodoList } from './components/TodoList'
+
+const reducer = combineReducers({
+  tasks: tasks.reducer
+})
+
+const store = configureStore({ reducer })
+
 export const App = () => {
-  return <div>Find me in App.jsx!</div>;
+  return (
+    <Provider store={store}>
+      <TodoList />
+    </Provider>
+  )
 };
